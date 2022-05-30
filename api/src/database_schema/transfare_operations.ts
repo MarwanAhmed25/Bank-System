c431c44d670f5a366f82d8d87b9eb52fef932f3c
marwanahmed25 <marwan4125881@gmail.com>
2022-05-30 16:38:27 +0200

    init

---
+++ api/src/database_schema/transfare_operations.ts
@@ -0,0 +1,48 @@
+import { DataTypes } from "sequelize";
+import Account from '../database';
+
+const operations = {
+    to_who: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            isNumeric: true
+        }
+      
+    },
+    account_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+            isNumeric: true
+        }
+    },
+    from_or_to: { // 0 means send to another account, 1 means charge to my account
+        type: DataTypes.BOOLEAN,
+        default: false,
+        allowNull: false,
+    },
+    balance_before: {
+        type: DataTypes.FLOAT,
+        default: 0,
+        allowNull: false,
+        validate:{
+            min:0
+        }
+    },
+    balance_after: {
+        type: DataTypes.FLOAT,
+        default: 0,
+        allowNull: false,
+        validate:{
+            min:0
+        }
+        
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+}
+    
+export default operations;   
