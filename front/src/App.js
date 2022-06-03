c3f51b4da893bb36dfcf7d076655d296a3ae5662
Mahmoud fahmy <mafahmypc@hotmail.com>
2022-06-03 13:21:35 +0200

    installed mui created a responsive side bar for Admin dashboard created layout for information

--- front/src/App.js
+++ front/src/App.js
@@ -6,68 +6,27 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import AdminDashScreen from "./screens/AdminDashScreen";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+
+      {/* when Admin sign in will be redirected to Admin dashboard */}
+      <AdminDashScreen />
+
       <main className="py-3">
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact></Route>
             <Route path="/login" element={<LoginScreen />}></Route>
             <Route path="/register" element={<RegisterScreen />}></Route>
-          </Routes>
 
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Counter />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <span>
-              <span>Learn </span>
-              <a
-                className="App-link"
-                href="https://reactjs.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className="App-link"
-                href="https://redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className="App-link"
-                href="https://redux-toolkit.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              ,<span> and </span>
-              <a
-                className="App-link"
-                href="https://react-redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-            </span>
-          </header>
+            {/* Will be Protected Route */}
+            <Route path="/adminPanel" element={<AdminDashScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
