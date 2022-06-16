a69719353adb437bec05053fcf10d7e829723c56
nohahakim <raafatabass@hotmail.com>
2022-06-16 17:30:18 +0200

    updated header file and authService file

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,39 +1,74 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../features/user/userSlice";
-import { useNavigate } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout, reset } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.auth);
 
   const onLogout = () => {
     dispatch(logout());
     dispatch(reset());
-    navigate("/");
+    navigate("/login");
   };
 
   return (
     <header>
-      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>Modern Bank</Navbar.Brand>
           </LinkContainer>
-
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {user ? (
-                <Nav.Link onClick={onLogout}> logout</Nav.Link>
+              {user && user.user?.role === "user" ? (
+                <NavDropdown title={user.user.name} id="username">
+                  <LinkContainer to="/user">
+                    <NavDropdown.Item>User Dashboard</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={onLogout}>Logout</NavDropdown.Item>
+                </NavDropdown>
               ) : (
+                //   <LinkContainer to="/login">
+                //     <Nav.Link>
+                //       <i className="fas fa-user"></i> Sign In
+                //     </Nav.Link>
+                //   </LinkContainer>
+                // )}
+                // {
+                user &&
+                user.user?.role === "admin" && (
+                  <NavDropdown title="Admin" id="adminmenu">
+                    <LinkContainer to="/admin/newrequest">
+                      <NavDropdown.Item>New Requests</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>User List</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/admin/accountlist">
+                      <NavDropdown.Item>Account List</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={onLogout}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                )
+              )}
+
+              {!user && (
                 <>
                   <LinkContainer to="/login">
-                    <Nav.Link>Sign in</Nav.Link>
+                    <Nav.Link>
+                      <i className="fas fa-user"></i> Sign In
+                    </Nav.Link>
                   </LinkContainer>
+
                   <LinkContainer to="/register">
                     <Nav.Link>
                       <i className="fas fa-user"></i> Register
